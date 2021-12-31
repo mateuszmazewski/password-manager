1d72b0808da97977e30c75a4ab8b6e71cd6a9e5c
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2021-12-31 19:06:16 +0100

    Change Server header, add Content-Security-Policy header, add comments about CSRF, XSS

--- src/main/java/com/github/mateuszmazewski/passwordmanager/security/SecurityConfiguration.java
+++ src/main/java/com/github/mateuszmazewski/passwordmanager/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @EnableWebSecurity
 @Configuration
@@ -24,9 +25,22 @@ public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    /*
+    All requests between the client and the server are included with a user session specific CSRF token.
+    All communication between the server and the client is handled by Vaadin,
+    so you do not need to remember to include and verify the CSRF tokens manually.
+
+    Vaadin has built-in protection against cross-site scripting (xss) attacks.
+    Vaadin uses Browser APIs that make the browser render content as text instead of HTML,
+    such as using innerText instead of innerHTML.
+    This negates the chance to accidentally inserting e.g. <script> tags into the DOM by binding unsecure string values.
+    Allowing insecure HTML content is never the default.
+     */
 
         super.configure(http);
         http.requiresChannel().antMatchers("/**").requiresSecure();
+        http.headers().addHeaderWriter(new StaticHeadersWriter("Server", "I am here to serve You"));
+        http.headers().contentSecurityPolicy("img-src 'self' https://website.vaadin.com");
         setLoginView(http, LoginView.class, LOGOUT_URL);
     }
 
